d517732c715e8716b2e9708d6fe2aa0590cc1382
Alex Sereda <sereda.san@gmail.com>
2022-07-17 17:03:54 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const pairs = {};
+  for (const [open, close] of bracketsConfig) {
+    pairs[close] = open;
+  }
+
+  const opens = Object.values(pairs);
+  const stack = [];
+
+  for (b of str) {
+    if (b === pairs[b] && b === stack[stack.length - 1]) {
+      stack.pop();
+      continue;
+    }
+    if (opens.includes(b)) {
+      stack.push(b);
+      continue;
+    }
+    if (!(stack.pop() === pairs[b])) {
+      return false;
+    }
+  }
+
+  return !stack.length;
+};
